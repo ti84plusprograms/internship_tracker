da766b84b3e280c6f746ecb4c2357d82f1bebe19
Agarwal, Aaryaman <aagarwal485@gatech.edu>
2025-02-22 15:08:33 -0500

    testing new dataset + better sampling procedures

--- ML_Modeling_binary_email_classifier/emailClassifier/emailClassifier.py
+++ ML_Modeling_binary_email_classifier/emailClassifier/emailClassifier.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
 
 app = Flask(__name__)
 CORS(app)
@@ -10,13 +13,15 @@ CORS(app)
 model = joblib.load('/app/email_classifier.pkl')  # Update with your model path
 vectorizer = joblib.load('/app/tfidf_vectorizer.pkl')  # Update with your vectorizer path
 
+print(len(vectorizer.vocabulary_))
+
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json(force=True)
-        subject = data['subject']
-        body = data['body']
-        
+        subject = clean_text(data['subject'])
+        body = clean_text(data['body'])
+
         # Combine subject and body and vectorize
         input_vector = vectorizer.transform([subject + ' ' + body])
         prediction = model.predict(input_vector)
@@ -24,6 +29,22 @@ def predict():
         return jsonify({'prediction': int(prediction[0])})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+def clean_text(text):
+    import nltk
+    nltk.download('punkt', quiet=True)  # Ensure it's downloaded
+    nltk.download('stopwords', quiet=True)
+    # Tokenize and lower case the text
+    text = text.lower()
+    tokens = word_tokenize(text)
+    
+    # Remove stopwords and punctuation
+    stop_words = set(stopwords.words('english'))
+    cleaned_tokens = [word for word in tokens if word not in stop_words and word not in string.punctuation]
+    
+    return " ".join(cleaned_tokens)
+
+# Apply the cleaning function to both 'email subject' and 'email body'
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
